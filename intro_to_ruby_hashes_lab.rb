205c4f13fe14b1c14f33690e13ac7faacc0f6087
Luis Herrera <herrera.lrh@gmail.com>
2019-10-29 02:27:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,3 +17,13 @@ def monopoly_with_second_tier
     :railroads => {pieces: 4, names: {}, rent_in_dollars: {} }
   }
 end
+
+def monopoly_with_third_tier
+  monopoly = {
+    :railroads => {pieces: 4,
+      names: {},
+      rent_in_dollars: {
+        one_piece_owned: 25
+        } }
+  }
+end
